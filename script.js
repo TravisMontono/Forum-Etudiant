2f18d855550bf449fec49f443a10592dc94e9c42
IKOULE <japhetikoume@gmail.com>
2025-04-06 20:47:44 +0100

    ajout du code menu

--- script.js
+++ script.js
@@ -1,50 +1,89 @@
-const tabs = document.querySelectorAll('.tab');
-const formContainers = document.querySelectorAll('.form-container');
+const tabs = document.querySelectorAll(".tab");
+const formContainers = document.querySelectorAll(".form-container");
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        tabs.forEach(t => t.classList.remove('active'));
-        tab.classList.add('active');
-        const tabId = tab.dataset.tab;
-        formContainers.forEach(container => {
-            container.style.display = container.id === tabId ? 'block' : 'none';
-        });
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((t) => t.classList.remove("active"));
+    tab.classList.add("active");
+    const tabId = tab.dataset.tab;
+    formContainers.forEach((container) => {
+      container.style.display = container.id === tabId ? "block" : "none";
     });
+  });
 });
 
+const navLinks = document.querySelectorAll(".nav-link");
+const pages = document.querySelectorAll(".page");
+const sidebar = document.getElementById("sidebar");
 
-const navLinks = document.querySelectorAll('.nav-link');
-const pages = document.querySelectorAll('.page');
-const sidebar = document.getElementById('sidebar');
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const pageId = link.dataset.page;
 
-navLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-        const pageId = link.dataset.page;
+    navLinks.forEach((navLink) => navLink.classList.remove("active"));
+    pages.forEach((page) => (page.style.display = "none"));
 
-        navLinks.forEach(navLink => navLink.classList.remove('active'));
-        pages.forEach(page => page.style.display = 'none');
-
-        document.getElementById(pageId).style.display = 'block';
-        link.classList.add('active');
-
-    });
+    document.getElementById(pageId).style.display = "block";
+    link.classList.add("active");
+  });
 });
 
+// dark mode
+function dark_mode() {
+  let bouton = document.getElementById("toggle-dark");
+  let body = document.body;
+  let navbar = document.getElementById("menu");
+  bouton.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+  });
+}
 
+dark_mode();
 
-    //dark mode 
+// desactivation  du champ recherche gauche  sur le premiere onglet
 
-    let bouton = document.getElementById("toggle-dark");
-    let body = document.body;
-    let navbar = document.getElementById("menu")
-    bouton.addEventListener("click", function() {
-      body.classList.toggle('dark-mode');
-     });
+let tab_1 = document.getElementById("tab-1");
+let tab_2 = document.getElementById("tab-2");
+let tab_3 = document.getElementById("tab-3");
+let input_search = document.getElementById("search");
+window.addEventListener("DOMContentLoaded", () => {
+  input_search.disabled = true;
+  input_search.style.cursor = "not-allowed";
+});
 
-    
-    // changement de container du bouton a moins 998px
-    /*
+tab_1.addEventListener("click", function () {
+  input_search.disabled = true;
+  input_search.style.cursor = "not-allowed";
+});
+
+tab_2.addEventListener("click", function () {
+  input_search.disabled = false;
+  input_search.style.cursor = "text";
+});
+
+tab_3.addEventListener("click", function () {
+  input_search.disabled = false;
+  input_search.style.cursor = "text";
+});
+
+let bouton = document.getElementById('navbar-toggler')
+let menu = document.getElementById('menu-right')
+
+bouton.addEventListener("click", function(){
+     menu.classList.toggle('active')
+})
+
+/*
+let nav_links = document.querySelectorAll('.nav-link')
+let menu_remove = document.getElementById('menu-right');
+ nav_links.addEventListener("click", function(){
+    menu_remove.classList.remove('active')
+ })
+*/
+
+// changement de container du bouton a moins 998px
+/*
      function lets(){
 
      let bouton = document.getElementById('ret')
